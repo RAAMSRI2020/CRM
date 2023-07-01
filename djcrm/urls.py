f6ff6e08f9572085a8f888361a425901e0b8cf08
RAAMSRI2020 <sriraam210903@gmail.com>
2023-07-01 23:50:09 +0530

    Completed leads list and detail view

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from leads.views import *
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home_page)
+    path('leads/',include('leads.urls',namespace='leads'))
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
-def home_page(request):
+def lead_list(request):
+    leads=Lead.objects.all()
+    context={
+        "leads":leads
+    }
     # return HttpResponse("Hello, world!")
-    return render(request,"leads/second_page.html")
+    return render(request,"leads/lead_list.html",context)
+
+def lead_detail(request,pk):
+    # print(pk)
+    lead=Lead.objects.get(id=pk)
+    context={
+        "lead":lead
+    }
+    return render(request,"leads/lead_detail.html",context)
